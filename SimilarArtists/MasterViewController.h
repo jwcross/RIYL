3c33195b497795527e7d701cc0ff319101aedc44
jwcross <jonathan.crossley@waterfield.com>
2014-09-01 23:54:12 -0500

    Initialize table view with sample artists

--- SimilarArtists/MasterViewController.h
+++ SimilarArtists/MasterViewController.h
@@ -9,11 +9,7 @@
 #import <UIKit/UIKit.h>
 #import <CoreData/CoreData.h>
 
-@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
-
-@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
-@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
-
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate, UITableViewDataSource, UITableViewDelegate>
 
 @end
 
--- SimilarArtists/MasterViewController.m
+++ SimilarArtists/MasterViewController.m
@@ -9,15 +9,25 @@
 #import "MasterViewController.h"
 #import "DetailViewController.h"
 #import "LastfmAPIClient.h"
+#import "Artist.h"
 
 @interface MasterViewController ()
-
+@property NSMutableArray *artists;
 @end
 
 @implementation MasterViewController
-            
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self fetchAllArtists];
+    [self.tableView reloadData];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
     
     // edit button
     self.navigationItem.leftBarButtonItem = self.editButtonItem;
@@ -27,13 +37,40 @@
                                               initWithBarButtonSystemItem:UIBarButtonSystemItemAdd
                                               target:self action:nil];
     
-    LastfmAPIClient *client = [LastfmAPIClient sharedClient];
-    [client getSimilarArtistsForArtist:@"Nujabes" limit:20 autocorrect:YES
-                               success:^(NSURLSessionDataTask *task, id responseObject) {
-                                   NSLog(@"Success -- %@", responseObject);
-                               } failure:^(NSURLSessionDataTask *task, NSError *error) {
-                                   NSLog(@"Failure -- %@", error);
-                               }];
+//    LastfmAPIClient *client = [LastfmAPIClient sharedClient];
+//    [client getSimilarArtistsForArtist:@"Nujabes" limit:20 autocorrect:YES
+//                               success:^(NSURLSessionDataTask *task, id responseObject) {
+//                                   NSLog(@"Success -- %@", responseObject);
+//                               } failure:^(NSURLSessionDataTask *task, NSError *error) {
+//                                   NSLog(@"Failure -- %@", error);
+//                               }];
+}
+
+#pragma mark - UITableViewDatasource
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.artists.count;
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"
+                                                            forIndexPath:indexPath];
+    // configure cell
+    Artist *artist = self.artists[indexPath.row];
+    cell.textLabel.text = artist.name;
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+#pragma mark - Private helper methods
+
+-(void)fetchAllArtists {
+    // Fetch entities with MagicalRecord, sorted by ascending name
+    self.artists = [[Artist findAllSortedBy:@"name" ascending:YES] mutableCopy];
 }
 
 @end

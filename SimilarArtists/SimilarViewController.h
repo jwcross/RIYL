9c6600ecee761a0cd62319b4c11642486a24feec
jwcross <jonathan.crossley@waterfield.com>
2014-09-03 19:50:16 -0500

    Add SimilarArtists collection view and controller

---
+++ SimilarArtists/SimilarViewController.h
@@ -0,0 +1,13 @@
+//
+//  SimilarViewController.h
+//  SimilarArtists
+//
+//  Created by Jonathan Crossley on 9/3/14.
+//  Copyright (c) 2014 CCS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SimilarViewController : UICollectionViewController
+
+@end
